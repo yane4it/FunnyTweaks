a16f0c949771062366b9cebc1fdbb0b928cef82e
yane4it <yaneit934@gmail.com>
2026-06-08 01:24:29 +0300

    Patched partialTicksModifier

--- src/main/java/dosliz/funnytweaks/FunnyTweaks.java
+++ src/main/java/dosliz/funnytweaks/FunnyTweaks.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class FunnyTweaks {
     public static final String MODID = "funnytweaks";
     public static final String NAME = "FunnyTweaks";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
